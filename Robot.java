a72d2d3d915f993071788a5b925f4db26cbbef6c
Markintus-Morris <87951221+Kinto55@users.noreply.github.com>
2022-04-11 00:42:07 -0400

    Add files via upload

---
+++ Robot.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Plumbing here.
+ *
+ * @author (Markintus Morris)
+ * @version (1/26/21)
+ */
+public class Robot
+{
+    //This is Robot
+    public static void main(String[] args)
+    { 
+        //We are creating a Drippy robot 
+        System.out.println("----------");
+        //Robot features Pretty Face 
+        System.out.println("(| o o |");
+        //More robot feature Body 
+        System.out.println(" (| ^ |)");
+        //Robot feature Legs
+        System.out.println(" |[   ]| ");
+        //Robots Shoe Game 
+        System.out.println("___________");
+        
+    }
+}
